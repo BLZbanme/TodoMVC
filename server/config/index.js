8d566f86082b3ec4a6b5a79c4490e253e5d76ead
BLZbanme <swordcome@gmail.com>
2020-07-05 23:17:31 +0800

    add config.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,27 +1,31 @@
-const secertKey = 'bytedance';
-const dbName = 'todomvc';
-const username = 'root';
-const password = '123456';
-const ip = 'localhost';
-const port = 3307;
+// const secertKey = 'bytedance';
+// const dbName = 'todomvc';
+// const username = 'root';
+// const password = '123456';
+// const ip = 'localhost';
+// const port = 3307;
 
-const sessionConfig = {
-    key: 'koa:sess',
-    mazAge: 5 * 60 * 1000,
-    autoCommit: true,
-    overwrite: true,
-    httpOnly: true,
-    signed: true,
-    rolling: true,
-    renew: false
-}
+// const sessionConfig = {
+//     key: 'koa:sess',
+//     mazAge: 5 * 60 * 1000,
+//     autoCommit: true,
+//     overwrite: true,
+//     httpOnly: true,
+//     signed: true,
+//     rolling: true,
+//     renew: false
+// }
 
-module.exports = {
-    secertKey,
-    dbName,
-    username,
-    password,
-    ip,
-    port,
-    sessionConfig
-};
+// module.exports = {
+//     secertKey,
+//     dbName,
+//     username,
+//     password,
+//     ip,
+//     port,
+//     sessionConfig
+// };
+
+const config = require('../../config.js');
+console.log(config);
+module.exports = config;
